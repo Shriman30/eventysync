6b5e03ba8656a4fbaf0ad3b23552320976bf26cc
Shriman Vinayagamoorthy <shriman30@hotmail.com>
2023-10-17 22:37:05 -0400

    Updated App.js
    Fixed UI transitions for authenticated and unauthenticated users

--- App.js
+++ App.js
@@ -1,29 +1,60 @@
-import React from "react";
-import { LogBox } from "react-native";
+import React, {useState, useEffect} from "react";
+import { ActivityIndicator, StyleSheet, View, LogBox } from "react-native";
 import WelcomeScreen from "./src/Screens/WelcomeScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { getAuth } from "firebase/auth";
 import LoginScreen from "./src/Screens/Auth/LoginScreen";
 import RegistrationScreen from "./src/Screens/Auth/RegistrationScreen";
 import Index from "./src/Screens/App";
 import EventDetails from "./src/Screens/App/EventDetails";
-import LoadingScreen from "./src/Components/LoadingScreen";
 
 const Stack = createNativeStackNavigator();
 
 LogBox.ignoreAllLogs(); // to ignore all warnings
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+      setIsLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Loading">
-        <Stack.Screen name="Loading" component={LoadingScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegistrationScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Main" component={Index} options={{ headerShown: false }} />
-        <Stack.Screen name="EventDetails" component={EventDetails} options={{ headerShown: false }} />
-      </Stack.Navigator>
+      {isLoading ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#F2E8A2"/>
+        </View>
+      ) : (
+        <Stack.Navigator initialRouteName={user ? "Main" : "Welcome"}>
+          <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false, gestureDirection:'horizontal', gestureEnabled:'false'}} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Register" component={RegistrationScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Main" component={Index} options={{ headerShown: false, gestureDirection:'horizontal', gestureEnabled:'false' }} />
+          <Stack.Screen name="EventDetails" component={EventDetails} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "rgba(11,37,69,0.90)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
